698fd055b4bd9946927a75821e37471ace8f0ffc
atjhoendz <achunarmando@gmail.com>
2020-06-20 21:41:48 +0700

    add comments

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,8 +1,22 @@
+/*
+* deklarasi main package, dalam suatu projek harus ada 1 main package
+* package nama-package
+ */
 package main
 
+/*
+* mengimport package lain
+* import "nama lib"
+* fmt => package I/O
+ */
 import "fmt"
 
+/*
+* fungsi main
+* fungsi yang bakal dijalankan pertama kali
+* deklarasi fungsi main harus pada main package
+ */
 func main() {
-	// print hello world
-	fmt.Println("Hello world")
+	// print hello world with newline
+	fmt.Println("Hello World!")
 }
