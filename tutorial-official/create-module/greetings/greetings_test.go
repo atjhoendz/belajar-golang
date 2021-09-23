df35df620731f4612d15d1d6ba5b0f917111aebf
atjhoendz <achunarmando@gmail.com>
2021-09-23 11:01:20 +0700

    official-tutorial: add greeting test

---
+++ tutorial-official/create-module/greetings/greetings_test.go
@@ -0,0 +1,25 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Armando"
+
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Armando")
+
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Armando") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "" error`, msg, err)
+	}
+}
